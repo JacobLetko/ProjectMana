de9a96d38e5c0ab478ea046c321a79095987e05d
Jacob Letko <s179061@students.aie.edu.au>
2018-06-04 10:43:40 -0700

    UI done??

--- Assets/ui/PauseScript.cs
+++ Assets/ui/PauseScript.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PauseScript : MonoBehaviour
 {
     public GameObject wheel;
     public GameObject menu;
-    public GameObject player;
     public GameObject HUD;
-
     public bool paused;
+
+    [Header("stats stuff")]
+    public GameObject player;
+    public GameObject healthText, manaText, StrText, DexText, ConText, IntText, WisText, ChaText, ExpText, LevelText;
 
     void Start()
     {
@@ -26,6 +29,7 @@ public class PauseScript : MonoBehaviour
             paused = true;
             player.GetComponent<PlayerController>().PausePlayer(paused);
             cursorstate(true);
+            getStats();
             Time.timeScale = 0.0f;
             HUD.gameObject.SetActive(false);
             menu.gameObject.SetActive(true);
@@ -68,5 +72,22 @@ public class PauseScript : MonoBehaviour
     private void cursorstate(bool state)
     {
         Cursor.visible = state;
+    }
+
+    private void getStats()
+    {
+        AbilityScore stats = player.GetComponent<AbilityScore>();
+
+        healthText.GetComponent<TextMeshProUGUI>().text = "HP: " + stats.abilities.Health.ToString();
+        manaText.GetComponent<TextMeshProUGUI>().text = "MP: " + stats.abilities.Mana.ToString();
+        ExpText.GetComponent<TextMeshProUGUI>().text = "EXP: " + stats.abilities.Experience.ToString();
+        LevelText.GetComponent<TextMeshProUGUI>().text = "Level: " + stats.Level.ToString();
+
+        StrText.GetComponent<TextMeshProUGUI>().text = "Str: " + stats.abilities.Str.ToString();
+        ConText.GetComponent<TextMeshProUGUI>().text = "Con: " + stats.abilities.Con.ToString();
+        DexText.GetComponent<TextMeshProUGUI>().text = "Dex: " + stats.abilities.Dex.ToString();
+        IntText.GetComponent<TextMeshProUGUI>().text = "Int: " + stats.abilities.Int.ToString();
+        WisText.GetComponent<TextMeshProUGUI>().text = "Wis: " + stats.abilities.Wis.ToString();
+        ChaText.GetComponent<TextMeshProUGUI>().text = "Cha: " + stats.abilities.Cha.ToString()
     }
 }
