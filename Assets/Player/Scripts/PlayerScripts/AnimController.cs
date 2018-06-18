818c734301d0c904a0e65ad365de029e68d3a452
Paul Gibbons <s179054@students.aie.edu.au>
2018-06-18 16:52:36 -0700

    add player animations

--- Assets/Player/Scripts/PlayerScripts/AnimController.cs
+++ Assets/Player/Scripts/PlayerScripts/AnimController.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class AnimController : MonoBehaviour
 {
     private Animator _animator;
+    private PlayerMovement _playerMovement;
     // Use this for initialization
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _playerMovement = FindObjectOfType<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Vertical") >= 0)
+        if (Input.GetAxisRaw("Vertical") > 0)
         {
-            _animator.Play("FastRun");
+            _animator.SetBool("isRunning", true);
         }
+        else
+        {
+            _animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            _animator.SetBool("isShooting", true);
+        }
+        else
+        {
+            _animator.SetBool("isShooting", false);
+        }
+
+ 
+        _animator.SetBool("isJumping", _playerMovement.isJumping);
+
+
+
+
     }
 }
